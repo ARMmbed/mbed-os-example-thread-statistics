942d27c7c94f3ac4a41033fdc9b39b74917bfbf9
Rajkumar Kanagaraj <45570536+rajkan01@users.noreply.github.com>
2020-02-05 10:15:33 +0000

    Make README more useful (#47)

--- main.cpp
+++ main.cpp
@@ -21,13 +21,42 @@
 #endif
  
 #define MAX_THREAD_STATS    0x8
- 
+#define BLINKY_THREAD_STACK 224
+#define IDLE_THREAD_STACK 384
+#define STOP_FLAG 0xEF
+#define WAIT_TIME_MS 500
+
+// Initialise the digital pin LED1 as an output
+DigitalOut led1(LED1);
+static EventFlags idle_ef;
+
+void blinky()
+{
+    while (1) {
+        led1 = !led1;
+        thread_sleep_for(WAIT_TIME_MS);
+    }
+}
+
+void idle()
+{
+    idle_ef.wait_all(STOP_FLAG);
+}
+
 int main()
 {
+    Thread *blinky_thread = new Thread(osPriorityNormal, BLINKY_THREAD_STACK, nullptr, "blinky_thread");
+    blinky_thread->start(blinky);
+
+    Thread *idle_thread = new Thread(osPriorityNormal, IDLE_THREAD_STACK, nullptr, "idle_thread");
+    idle_thread->start(idle);
+
+    // Sleep helps other created threads to run
+    thread_sleep_for(10);
     mbed_stats_thread_t *stats = new mbed_stats_thread_t[MAX_THREAD_STATS];
     int count = mbed_stats_thread_get_each(stats, MAX_THREAD_STATS);
-    
-    for(int i = 0; i < count; i++) {
+
+    for (int i = 0; i < count; i++) {
         printf("ID: 0x%x \n", stats[i].id);
         printf("Name: %s \n", stats[i].name);
         printf("State: %d \n", stats[i].state);
@@ -36,5 +65,8 @@ int main()
         printf("Stack Space: %d \n", stats[i].stack_space);
         printf("\n");
     }
+   
+    blinky_thread->terminate();
+    idle_thread->terminate();    
     return 0;
 }
