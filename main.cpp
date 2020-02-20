9d607adfdd65878318b3692ee4f4d8e435f98191
Evelyne Donnaes <evelyne.donnaes@arm.com>
2020-02-20 11:49:39 +0000

    Updated configuration parameters (#50)
    Updated README with a configuration section.
    Modified mbed_app.json to use platform.thread-stats-enabled instead of the MBED_THREAD_STATS_ENABLED macro.
    Fixed warnings.

--- main.cpp
+++ main.cpp
@@ -15,9 +15,10 @@
 * limitations under the License.
 */
 #include "mbed.h"
+#include <inttypes.h>
  
 #if !defined(MBED_THREAD_STATS_ENABLED)
-#error "Stats not enabled"
+#error "Thread statistics not enabled"
 #endif
  
 #define MAX_THREAD_STATS    0x8
@@ -57,12 +58,12 @@ int main()
     int count = mbed_stats_thread_get_each(stats, MAX_THREAD_STATS);
 
     for (int i = 0; i < count; i++) {
-        printf("ID: 0x%x \n", stats[i].id);
+        printf("ID: 0x%" PRIx32 "\n", stats[i].id);
         printf("Name: %s \n", stats[i].name);
-        printf("State: %d \n", stats[i].state);
-        printf("Priority: %d \n", stats[i].priority);
-        printf("Stack Size: %d \n", stats[i].stack_size);
-        printf("Stack Space: %d \n", stats[i].stack_space);
+        printf("State: %" PRId32 "\n", stats[i].state);
+        printf("Priority: %" PRId32 "\n", stats[i].priority);
+        printf("Stack Size: %" PRId32 "\n", stats[i].stack_size);
+        printf("Stack Space: %" PRId32 "\n", stats[i].stack_space);
         printf("\n");
     }
    
